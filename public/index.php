3d928048905d5bfc1fedd677640cfab70df6c844
Chenguang Li <lcg224@students.cs.ubc.ca>
2023-08-07 07:16:41 +0800

    create non player character page

--- public/index.php
+++ public/index.php
@@ -19,19 +19,20 @@ $summoners = $query->fetchAll(PDO::FETCH_ASSOC);
     <style>
         body { background-color: #f5f5f5; }
         .navbar { background-color: #ffcc00; }
-        .jumbotron { background-image: url('images/banner.jpg'); background-size: cover; color: white; }
+        .jumbotron { background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ec6b653d-20ae-41d7-8e78-5a2ec054db72/d7nzyi4-717deda0-3474-46d1-83d5-4f064cd966ad.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2VjNmI2NTNkLTIwYWUtNDFkNy04ZTc4LTVhMmVjMDU0ZGI3MlwvZDduenlpNC03MTdkZWRhMC0zNDc0LTQ2ZDEtODNkNS00ZjA2NGNkOTY2YWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.W39a-Uy9Isacf1cePghCwVuwqU3Qe2R44yLdJ3ZqWXw'); background-size: cover; color: white; }
         .feature { padding: 20px; }
     </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#">LOL Platform</a>
+        <a class="navbar-brand" href="index.php">LOL Platform</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+                <li class="nav-item"><a class="nav-link" href="ingame.php">In Game Info</a></li>
                 <li class="nav-item"><a class="nav-link" href="champions.php">Champions</a></li>
                 <li class="nav-item"><a class="nav-link" href="store.php">Store</a></li>
             </ul>
