ae8ace4a063073e8939b3693d08198de3d2e3147
Tengs Penkwe <tengspenkwe@gmail.com>
2023-08-06 19:31:52 -0700

    make the pages of champion more beautiful

--- public/champion.php
+++ public/champion.php
@@ -1,25 +1,72 @@
 <?php
 require '../config/database.php';
 
-$champion_name = $_GET['name'] ?? ''; // Retrieve the champion name from the GET parameter
-
-// Query the ChampionBCNF table to get details of the champion
+$champion_name = $_GET['name'] ?? '';
 $db = getDBConnection();
-$query = $db->prepare('SELECT * FROM ChampionBCNF WHERE name = :name');
+$query = $db->prepare('
+    SELECT c.*, e.background_story 
+    FROM ChampionBCNF c 
+    LEFT JOIN EpithetBackground e 
+    ON c.epithet = e.epithet 
+    WHERE name = :name
+');
 $query->execute([':name' => $champion_name]);
 
 $champion = $query->fetch(PDO::FETCH_ASSOC);
+
 if (!$champion) {
     echo 'Champion not found!';
     exit;
 }
-
-// Display the champion details
-echo "<h1>{$champion['name']}</h1>";
-echo "<p>Cost: {$champion['cost']}</p>";
-echo "<p>Region: {$champion['region']}</p>";
-echo "<p>Epithet: {$champion['epithet']}</p>";
-
-// You can add more details such as abilities, skins, and other related information using additional queries.
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($champion['name']) ?></title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            background-color: #f4f4f4;
+        }
+        .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #666;
+            margin: 10px 0;
+        }
+        .back-link {
+            display: block;
+            margin: 20px 0;
+            text-decoration: none;
+            color: #0066cc;
+        }
+        .back-link:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1><?= htmlspecialchars($champion['name']) ?></h1>
+        <p>Cost: <?= $champion['cost'] ?></p>
+        <p>Region: <?= $champion['region'] ?></p>
+        <p>Epithet: <?= $champion['epithet'] ?></p>
+        <h2>Background Story:</h2>
+        <p><?= htmlspecialchars($champion['background_story']) ?></p>
+        <!-- Add more details such as abilities, skins, and other related information using additional queries here -->
+        <a class="back-link" href="champions.php">Back to Champions List</a>
+    </div>
+</body>
+</html>
--- public/champions.php
+++ public/champions.php
@@ -7,15 +7,51 @@ $query = $db->prepare('SELECT * FROM ChampionBCNF');
 $query->execute();
 
 $champions = $query->fetchAll(PDO::FETCH_ASSOC);
-
-// Display all champions and link to their individual detail pages
-echo "<h1>Champions List</h1>";
-echo "<ul>";
-foreach ($champions as $champion) {
-    echo "<li>";
-    echo "<a href='champions.php?name=" . urlencode($champion['name']) . "'>" . htmlspecialchars($champion['name']) . "</a>";
-    echo "</li>";
-}
-echo "</ul>";
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Champions List</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            padding: 10px;
+            border: 1px solid #ccc;
+            margin: 8px 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        a {
+            text-decoration: none;
+            color: #0066cc;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h1>Champions List</h1>
+    <ul>
+        <?php foreach ($champions as $champion): ?>
+            <li>
+                <a href="champion.php?name=<?= urlencode($champion['name']) ?>"><?= htmlspecialchars($champion['name']) ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
