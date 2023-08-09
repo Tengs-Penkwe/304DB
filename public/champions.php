f81f8b4d9fa408f84fcfa6c5428a0d25529440bf
Tengs Penkwe <tengspenkwe@gmail.com>
2023-08-08 19:58:20 -0700

    implement GROUP BY WITH HAVING

--- public/champions.php
+++ public/champions.php
@@ -9,10 +9,21 @@ $query->execute();
 $champions = $query->fetchAll(PDO::FETCH_ASSOC);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $key = $_POST['key'];
-    $query = $db->prepare("SELECT champion_name FROM AbilityOwned WHERE key = :key GROUP BY champion_name");
-    $query->execute(['key' => $key]);
-    $champions_with_ability = $query->fetchAll(PDO::FETCH_ASSOC);
+    if (isset($_POST['minAbilities'])) {
+        $minAbilities = $_POST['minAbilities'];
+        $query = $db->prepare('SELECT champion_name, COUNT(*) as ability_count FROM AbilityOwned GROUP BY champion_name HAVING COUNT(*) > :minAbilities');
+        $query->bindParam(':minAbilities', $minAbilities, PDO::PARAM_INT);
+        $query->execute();
+        $champions_with_abilities = $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // DIVISION query
+    if (isset($_POST['key'])) {
+        $key = $_POST['key'];
+        $query = $db->prepare("SELECT champion_name FROM AbilityOwned WHERE key = :key GROUP BY champion_name");
+        $query->execute(['key' => $key]);
+        $champions_with_ability = $query->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 ?>
 
@@ -94,6 +105,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </li>
             <?php endforeach; ?>
         </ul>
+
+        <h2>Find Champions with Specific Number of Abilities</h2>
+        <form action="" method="post">
+            <label for="minAbilities">Minimum Number of Abilities:</label>
+            <input type="number" id="minAbilities" name="minAbilities" required>
+            <button type="submit">Find Champions</button>
+        </form> 
+        <?php if (isset($champions_with_abilities)): ?>
+        <h3>Champions with More Than Specified Abilities</h3>
+        <table border="1">
+            <tr>
+                <th>Champion Name</th>
+                <th>Number of Abilities</th>
+            </tr>
+            <?php foreach ($champions_with_abilities as $champion): ?>
+                <tr>
+                    <td><?= htmlspecialchars($champion['champion_name']) ?></td>
+                    <td><?= htmlspecialchars($champion['ability_count']) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php endif; ?>
     </div>
 </body>
 </html>
